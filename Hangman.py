982ac26c0102f7742c5715416a218c6f7e6c6945
darkgirlchan <danna129.dr@gmail.com>
2024-10-24 16:59:22 -0500

    code for name and age is eliminated and implemetate this funcion in hangman game

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,11 @@
+def main():
+    name = input("What is your name?")
+    age = input("How old are you?")
+    print(f"Hello, {name}!  You are {age}  years old.")
+
+if __name__ == "__main__":
+    main()
+
 import random
  
 def making_a_guess():
