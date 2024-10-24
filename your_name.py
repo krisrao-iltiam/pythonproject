1c70264b4fd3162ff6d1ad8bf610b8bc3baf33d8
darkgirlchan <danna129.dr@gmail.com>
2024-10-24 16:49:47 -0500

    code for name and age

---
+++ your_name.py
@@ -0,0 +1,8 @@
+
+def main():
+    name = input("What is your name?")
+    age = input("How old are you?")
+    print(f"Hello, {name}!  You are {age}  years old.")
+
+if __name__ == "__main__":
+    main()
